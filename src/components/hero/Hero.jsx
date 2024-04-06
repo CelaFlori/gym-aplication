6cb791ba5d4555504744c9cc879bf4cdefa4a266
Daniel <floricela999@gmail.com>
2024-04-06 15:02:51 +0200

    changes

--- src/components/hero/Hero.jsx
+++ src/components/hero/Hero.jsx
@@ -1,6 +1,10 @@
 import React from "react";
 import "./Hero.css";
 import Header from "./Header/Header";
+import hero_image from '../../assets/hero_image.png'
+import hero_image_back from '../../assets/hero_image_back.png'
+import Heart from '../../assets/heart.png'
+import Calories from '../../assets/calories.png'
 
 export default function hero() {
   return (
@@ -23,12 +27,49 @@ export default function hero() {
           <div>
             <span>
               In here we will help you to shape and build your ideal body and
-              your life to fullset
+              your life to fullest
             </span>
           </div>
         </div>
+        <div className='figures'>
+          <div>
+            <span>+140</span>
+            <span>expert coach</span>
+          </div>
+          <div>
+            <span>+978</span>
+            <span>members joined</span>
+          </div>
+          <div>
+            <span>+50</span>
+            <span> fitness programs</span>
+          </div>
+        </div>
+
+        <div className='hero-buttons'>
+          <button className='btn'>Get Started</button>
+          <button className='btn'>Learn More</button>
+        </div>
       </div>
-      <div className="right-h">right side</div>
+      <div className="right-h">
+        <button className='btn'>Join Now</button>
+
+        <div className='heart-rate'>
+          <img src={Heart} alt="Heart"/>
+          <span>Heart Rate</span>
+          <span>116 bmp</span>
+        </div>
+        <img src={hero_image} alt='' className='hero-image' />
+        <img src={hero_image_back} alt='' className='hero-image-back' />
+
+        <div className='calories'>
+          <img src={Calories} alt=""/>
+          <div>
+            <span>Calories Burned</span>
+            <span>220 kcal</span>
+          </div>
+        </div>
+      </div>
     </div>
   );
 }
