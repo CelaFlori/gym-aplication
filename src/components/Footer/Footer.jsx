2a6893d57ed9323d3c1de492685162c829073e42
Daniel <floricela999@gmail.com>
2024-04-07 22:52:53 +0200

    responsive section

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -10,14 +10,17 @@ export const Footer = () => {
             <hr />
             <div className='footer'>
                 <div className='social-links'>
-                <img src={Github} alt=""/>
-                <img src={Instagram} alt=""/>
-                <img src={LinkedIn} alt=""/>
+                    <img src={Github} alt=""/>
+                    <img src={Instagram} alt=""/>
+                    <img src={LinkedIn} alt=""/>
+                </div>
+                <div className='logo-f'>
+                    <img src={Logo} alt=""/>
+                </div>
+                <div className='blur blur-f-2'></div>
             </div>
-            <div className='logo-f'>
-                <img src={Logo} alt=""/>
-            </div>
-        </div>
+            <div className='blur blur-f-1'></div>
+
         </div>
     )
 }
